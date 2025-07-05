2d0b91084bccbe972979acda2f7274dc0d5bb762
Kameron Eves <kameron.eves@utahtech.edu>
2025-07-05 00:06:50 -0600

    last change didn't work, I didn't change the last line of code

--- DNE_log.py
+++ DNE_log.py
@@ -81,7 +81,7 @@ def log(message: str, type: str = "info"):
             # get file path
             current_file_path = os.path.abspath(__file__)
             current_dir = os.path.dirname(current_file_path)
-            root_dir = os.path.dirname(current_file_path)
+            root_dir = os.path.dirname(current_dir)
             filepath = os.path.join(root_dir,"_log.txt")
 
             # Get the current log file content
