46f01aa6b159f0499a0fd073c6d57bc2fd9207d8
Kameron Eves <kameron.eves@utahtech.edu>
2025-07-09 19:48:50 -0600

    fixes type error

--- DNE_AutoGrader.py
+++ DNE_AutoGrader.py
@@ -14,7 +14,6 @@ from datetime import datetime
 from collections import defaultdict
 import os
 import traceback
-from .DNE_Assignment import Assignment
 from .DNE_AutoGraderParam import AutoGraderParam
 from .DNE_log import log
 from .DNE_HiddenPrints import HiddenPrints
@@ -41,7 +40,7 @@ class AutoGrader():
                         "# The instructor will be notified if you submit an edited version of this file.",
                         "########################################################################################"]
 
-        def __init__(self, assignments: list[Assignment], Param: AutoGraderParam):
+        def __init__(self, assignments: list, Param: AutoGraderParam):
             # Save assignments
             self.assignments = assignments
 
